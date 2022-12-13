b95e5582d40bc436f1b0ea3f1aabb8eb9cc1ee06
AishaAnwar <aishaanwar938@gmail.com>
2022-12-13 22:39:31 +0500

    first commit

---
+++ src/pages/SellCar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import sellCar from "../assets/images/sell-car.png";
+import "../styles/sell-car.css";
+import TrackingChart from "../charts/TrackingChart";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
+const SellCar = () => {
+  const percentage = 55;
+  const percentage02 = 45;
+  
+  return (
+    <div className="sell__car">
+      <div className="sell__car-wrapper">
+        <h2 className="sell__car-title">Create Course</h2>
+        
+
+          </div>
+    </div>
+  );
+};
+
+export default SellCar;
